f27ee6bc1afbac295d29404662deab609a315141
ChrisNguyenVnhrisNguyenVN <nguyentuananh1997vn@gmail.com>
2022-09-20 22:26:48 +0700

    lam btvn 01_05

--- models/analytics/fact_sales_order_lines.sql
+++ models/analytics/fact_sales_order_lines.sql
@@ -1,23 +1,36 @@
-with sales_order_lines__source as (
+with fact_sales__order_lines as (
   select * from `duckdata-320210.wide_world_importers.sales__order_lines`
 )
-
-, sales_order_lines__rename as (
-  select
-   order_line_id as sales_order_line_id,
-   quantity,
-   unit_price
-  from sales_order_lines__source
+, fact_sales_order_lines__renamed_column as (
+  select 
+    order_line_id AS sales_order_line_id
+  , stock_item_id AS product_id
+  , quantity as quantity
+  , unit_price as unit_price
+  , quantity * unit_price AS gross_amount
+  from fact_sales__order_lines
 )
 
-, sales_order_lines__cast as (
+, fact_sales_order_lines__cast as (
+SELECT 
+  CAST(sales_order_line_id AS INT64) AS sales_order_line_id
+  , CAST(product_id AS INT64) AS product_id
+  , CAST(quantity AS NUMERIC) as quantity
+  , CAST(unit_price AS NUMERIC) as unit_price
+FROM fact_sales_order_lines__renamed_column
+)
+
+, fact_sales_order_lines__calculated as (
   select 
-    cast(sales_order_line_id as INT64) AS sales_order_line_id,
-    cast(quantity as numeric) as quantity,
-    cast(unit_price as numeric) as unit_price
-  from sales_order_lines__rename
+    *,
+    quantity*unit_price as gross_amount
+from fact_sales_order_lines__cast
 )
 
 select 
-  *
-from sales_order_lines__cast
+  sales_order_line_id,
+  product_id,
+  quantity,
+  unit_price,
+  gross_amount
+from fact_sales_order_lines__calculated
